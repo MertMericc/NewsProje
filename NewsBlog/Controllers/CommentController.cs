bae7e16a476b4dab188dde7e2a10d8a9d059c295
MERT MERİÇ <mericasd2051@gmail.com>
2022-05-25 03:05:44 +0300

    details düzeltildi

---
+++ NewsBlog/Controllers/CommentController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsBlog.Controllers
+{
+    public class CommentController : Controller
+    {
+        CommentManager commentManager = new CommentManager(new CommentRepository());
+        public IActionResult AddComment()
+        {
+            return View();
+        }
+
+        public IActionResult AddComment(Comment p)
+        {
+            
+            return View();
+        }
+    }
+}
